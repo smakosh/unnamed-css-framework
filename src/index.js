c449298cb634f5261a855ab5f4ade1490c59f583
Ismail Ghallou <ismai23l@hotmail.com>
2018-02-01 19:44:20 +0000

    added unnamed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import AppRoutes from './routes/routes'
 import registerServiceWorker from './registerServiceWorker'
 
+import 'unnamed'
 import './styles/main.css'
 import './styles/header.css'
 
